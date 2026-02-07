c117142d9175700fe8a19cf634dd966727d60404
harumaki1220 <haruma181220@gmail.com>
2026-02-08 00:02:30 +0900

    feat: ABC444 A問題をAC

---
+++ abc/abc444/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    if (N % 111 == 0)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
